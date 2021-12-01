4e856190f4dce126f957fc8bd4e6be78cdaad92d
tranhd95 <tranhd95@gmail.com>
2021-12-01 17:59:12 +0100

    Shorten 01.py

--- 01.py
+++ 01.py
@@ -1,28 +1,15 @@
+from itertools import pairwise
 from utils import iter_file, iter_lines, rinput
 
 
-def solve1():
-    it = iter_file(1)
-    prev = int(next(it))
-    cnt = 0
-    for line in it:
-        nxt = int(line)
-        if nxt > prev:
-            cnt += 1
-        prev = nxt
-    print(cnt)
+def solve1(lst):
+    print(sum(1 for fst, snd in pairwise(lst) if snd > fst))
 
+def solve2(lst):
+    sums = [sum(lst[i:i+3]) for i in range(len(lst)-2)]
+    print(solve1(sums))
 
-def solve2():
-    depths = list(map(int, iter_file(1)))
-    prev = sum(depths[0:3])
-    cnt = 0
-    for i in range(1, len(depths)-2):
-        nxt = sum(depths[i:i+3])
-        if nxt > prev:
-            cnt += 1
-        prev = nxt
-    print(cnt)
-        
-solve1()
-solve2()
+inp = list(map(int, iter_file(1)))
+
+solve1(inp)
+solve2(inp)
