fe7c76960735a0ab39ab9f446203eba763179460
tranhd95 <tranhd95@gmail.com>
2021-12-02 20:30:45 +0100

    Edit 01.py

--- 01.py
+++ 01.py
@@ -1,13 +1,15 @@
 from itertools import pairwise
-from utils import iter_file, iter_lines, rinput
+from utils import iter_file
 
 
 def solve1(lst):
-    print(sum(1 for fst, snd in pairwise(lst) if snd > fst))
+    print(sum(snd > fst for fst, snd in pairwise(lst)))
+
 
 def solve2(lst):
-    sums = [sum(lst[i:i+3]) for i in range(len(lst)-2)]
+    sums = [sum(lst[i : i + 3]) for i in range(len(lst) - 2)]
     print(solve1(sums))
+
 
 inp = list(map(int, iter_file(1)))
 
