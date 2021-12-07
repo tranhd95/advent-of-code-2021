d8d3b74c45bfce756976075fa04af857e07a0c92
tranhd95 <tranhd95@gmail.com>
2021-12-07 09:26:30 +0100

    Prettify

--- 07.py
+++ 07.py
@@ -1,25 +1,23 @@
+import sys
 from utils import rinput
 
-def solve(inp):
-    inp = sorted(inp)
+def solve1(inp):
     min_, max_ = min(inp), max(inp)
-    costs = []
+    min_cost = sys.maxsize
     for i in range(min_, max_+1):
-        costs.append(sum(abs(n - i) for n in inp))
-    print(min(costs))
+        min_cost = min(sum(abs(n - i) for n in inp), min_cost)
+    print(min_cost)
 
 def solve2(inp):
-    inp = sorted(inp)
     min_, max_ = min(inp), max(inp)
-    costs = []
+    min_cost = sys.maxsize
     for i in range(min_, max_+1):
-        costs.append(sum(sn(abs(n - i), 1, abs(n - i)) for n in inp))
-    print(min(costs))
+        min_cost = min(sum(sn(abs(n - i), 1, abs(n - i)) for n in inp), min_cost)
+    print(min_cost)
 
 def sn(n, a1, an):
     return n*(a1 + an)//2
 
-t = list(map(int, test.split(",")))
 inp = list(map(int, rinput(7).split(",")))
 
 solve1(inp)
