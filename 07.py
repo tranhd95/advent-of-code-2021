691abb7a79ea17240a0855c90b4772a6c03370d8
tranhd95 <tranhd95@gmail.com>
2021-12-07 08:28:24 +0100

    Fix

--- 07.py
+++ 07.py
@@ -1,30 +1,25 @@
 from utils import rinput
 
-
-def solve1(inp):
+def solve(inp):
     inp = sorted(inp)
     min_, max_ = min(inp), max(inp)
     costs = []
-    # for i in range(min_, max_ + 1):
-    # costs.append()
-    # print(min(costs))
-    res = min(sum(abs(n - i) for n in inp) for i in range(min_, max_ + 1))
-    print(res)
-
+    for i in range(min_, max_+1):
+        costs.append(sum(abs(n - i) for n in inp))
+    print(min(costs))
 
 def solve2(inp):
     inp = sorted(inp)
     min_, max_ = min(inp), max(inp)
     costs = []
-    for i in range(min_, max_ + 1):
+    for i in range(min_, max_+1):
         costs.append(sum(sn(abs(n - i), 1, abs(n - i)) for n in inp))
     print(min(costs))
 
-
 def sn(n, a1, an):
-    return n * (a1 + an) / 2
+    return n*(a1 + an)//2
 
-
+t = list(map(int, test.split(",")))
 inp = list(map(int, rinput(7).split(",")))
 
 solve1(inp)
